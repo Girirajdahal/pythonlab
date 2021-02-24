fc010a04d22900f09c2364d83913c4af7be71c38
prasiddahal <giriraj.dahal16@gmail.com>
2021-02-24 11:16:15 +0545

    Initial commit

---
+++ lab exercise 1/ques1_sum_of_3_nums.py
@@ -0,0 +1,6 @@
+#program that takes three numbers and print their sum. Every number is given on a separate line
+a= int(input("Enter the first number"))
+b= int(input("enter the second number"))
+c= int(input("Enter the third Number"))
+s= a+b+c
+print("the sum of three numbers is =",s)
