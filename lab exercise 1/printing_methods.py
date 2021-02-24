a13c754332d69f7e8fab004b70332c20a42d32c5
prasiddahal <giriraj.dahal16@gmail.com>
2021-02-24 11:41:14 +0545

    Methods of printing

---
+++ lab exercise 1/printing_methods.py
@@ -0,0 +1,7 @@
+name= input("Enter your name")
+age= int(input("Enter your age"))
+print(f"My name is {name} and my age is {age} ")
+print("My name is ",name, "and my age is", age)
+print("My name is"+ name + "and my age is " + str(age) )
+print("My name is %s and my age is %d " %(name,age))
+print("My name is {} and my age is {}".format(name,age))
